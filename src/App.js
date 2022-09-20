7d28cf5a341eafd5ad5ef1c1bdea5b331cbed203
Alexandr Zabrodskyi <zabrodskyi71011@gmail.com>
2022-09-20 16:26:34 +0300

    fix small bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import axios from 'axios'
-import Header from './components/Header'
-import Drawer from './components/Drawer'
-import Home from './pages/Home'
+import React from 'react';
+import axios from 'axios';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+import Home from './pages/Home';
 import { Route } from 'react-router-dom';
-import Favorites from './pages/Favorites'
-
+import Favorites from './pages/Favorites';
 
 function App() {
   const [items, setItems] = React.useState([]);
@@ -15,43 +14,52 @@ function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    axios.get('https://6323470d362b0d4e7de06fcd.mockapi.io/items').then(res => {
-      setItems(res.data)
-    })
-    axios.get('https://6323470d362b0d4e7de06fcd.mockapi.io/cart').then(res => {
-      setCartItems(res.data)
-    })
-    axios.get('https://6323470d362b0d4e7de06fcd.mockapi.io/favorites').then(res => {
-      setFavorites(res.data)
-    })
+    axios
+      .get('https://6323470d362b0d4e7de06fcd.mockapi.io/items')
+      .then((res) => {
+        setItems(res.data);
+      });
+    axios
+      .get('https://6323470d362b0d4e7de06fcd.mockapi.io/cart')
+      .then((res) => {
+        setCartItems(res.data);
+      });
+    axios
+      .get('https://6323470d362b0d4e7de06fcd.mockapi.io/favorites')
+      .then((res) => {
+        setFavorites(res.data);
+      });
   }, []);
 
-
-
   const onAddToCart = (obj) => {
-    axios.post('https://6323470d362b0d4e7de06fcd.mockapi.io/cart', obj)
-    setCartItems((prev) => [...prev, obj])
-  }
+    axios.post('https://6323470d362b0d4e7de06fcd.mockapi.io/cart', obj);
+    setCartItems((prev) => [...prev, obj]);
+  };
 
   const onRemoveItem = (id) => {
-    axios.delete(`https://6323470d362b0d4e7de06fcd.mockapi.io/cart/${id}`)
-    setCartItems((prev) => prev.filter(item => item.id !== id))
-  }
+    axios.delete(`https://6323470d362b0d4e7de06fcd.mockapi.io/cart/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
 
   const onChangeSearchInput = (event) => {
-    setSearchValue(event.target.value)
-  }
+    setSearchValue(event.target.value);
+  };
 
   const onAddToFavorite = (obj) => {
-    console.log(obj, 'obj')
-    axios.post('https://6323470d362b0d4e7de06fcd.mockapi.io/favorites')
-    setFavorites((prev) => [...prev, obj])
-  }
-
+    console.log(obj, 'obj');
+    axios.post('https://6323470d362b0d4e7de06fcd.mockapi.io/favorites');
+    setFavorites((prev) => [...prev, obj]);
+  };
 
   return (
     <div className="wrapper clear">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
+      {cartOpened && (
+        <Drawer
+          items={cartItems}
+          onClose={() => setCartOpened(false)}
+          onRemove={onRemoveItem}
+        />
+      )}
 
       <Header onClickCart={() => setCartOpened(true)} />
 
@@ -62,16 +70,14 @@ function App() {
           setSearchValue={setSearchValue}
           onChangeSearchInput={onChangeSearchInput}
           onAddToFavorite={onAddToFavorite}
-          onAddToCart={onAddToCart} />
+          onAddToCart={onAddToCart}
+        />
       </Route>
 
-      <Route path='/favorites' exact>
-        <Favorites
-          items={favorites} />
+      <Route path="/favorites" exact>
+        <Favorites items={favorites} />
       </Route>
-
     </div>
-
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,38 +1,55 @@
-import styles from './Card.module.scss'
-import React from 'react'
+import styles from './Card.module.scss';
+import React from 'react';
 
-function Card({ onFavorite, title, price, imageUrl, onPlus }) {
-    const [isAdded, setIsAdded] = React.useState(false);
-    const [isFavorite, setIsFavorite] = React.useState(false);
+function Card({ id, title, imageUrl, price, onFavorite, onPlus }) {
+  const [isAdded, setIsAdded] = React.useState(false);
+  const [isFavorite, setIsFavorite] = React.useState(false);
+  const obj = { id, parentId: id, title, imageUrl, price };
 
+  const onClickPlus = () => {
+    onPlus(obj);
+    setIsAdded(!isAdded);
+  };
 
-    const onClickPlus = () => {
-        onPlus({ title, price, imageUrl })
-        setIsAdded(!isAdded)
-    }
+  const onClickFavorite = () => {
+    onFavorite(obj);
+    setIsFavorite(!isFavorite);
+  };
 
-    const onClickFavorite = () => {
-        setIsFavorite(!isFavorite)
-    }
-
-    return (
-        <div className={styles.card}>
-            <div className={styles.favorite} onClick={onFavorite}>
-                <img onClick={onClickFavorite} alt="unliked" src={isFavorite ? "/react-sneakers/img/liked.svg" : "/react-sneakers/img/unliked.svg"} />
-            </div>
-            <img width={133} height={112} alt="sneakers" src={imageUrl} />
-            <h5>{title}</h5>
-            <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column">
-                    <span>Ціна: </span>
-                    <b>{price} грн.</b>
-                </div>
-                <div>
-                    <img className={styles.plus} onClick={onClickPlus} alt="plus" src={isAdded ? "/react-sneakers/img/btn-checked.svg" : "/react-sneakers/img/btn-plus.svg"} />
-                </div>
-            </div>
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite} onClick={onClickFavorite}>
+        <img
+          alt="unliked"
+          src={
+            isFavorite
+              ? '/react-sneakers/img/liked.svg'
+              : '/react-sneakers/img/unliked.svg'
+          }
+        />
+      </div>
+      <img width={133} height={112} alt="sneakers" src={imageUrl} />
+      <h5>{title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Ціна: </span>
+          <b>{price} грн.</b>
         </div>
-    )
+        <div>
+          <img
+            className={styles.plus}
+            onClick={onClickPlus}
+            alt="plus"
+            src={
+              isAdded
+                ? '/react-sneakers/img/btn-checked.svg'
+                : '/react-sneakers/img/btn-plus.svg'
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
